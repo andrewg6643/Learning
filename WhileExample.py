ea5ad4c2cd1ff458883a60ca651e92ea80338cc1
Andrew Gaskin <Andrew@Andrews-MacBook-Pro.local>
2020-02-16 08:27:10 -0500

    FristCommit

---
+++ WhileExample.py
@@ -0,0 +1,12 @@
+spam = 0
+getout = 0
+while spam <5:
+    if getout == 0:
+        print("Hello World")
+        spam = spam +1
+        if spam == 4:
+            getout = 1
+    elif getout == 1:
+        spam = 5
+        
+        
